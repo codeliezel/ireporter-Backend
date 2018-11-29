6a3724afe2feff339ae7e2d9bbfbf5f9e9e2e75b
funmi5 <35759534+funmi5@users.noreply.github.com>
2018-11-30 00:59:54 +0100

    Add the server files for iReporter
    This contains what is mainly used to develop the api endpoints

---
+++ server/db/redflags_table.js
@@ -0,0 +1,30 @@
+const redflag = [
+  {
+    id: 1,
+    createdOn: 'September 23, 2018',
+    createdBy: 'Funmi Olaiya',
+    email: 'funmi677@yahoo.com',
+    type: 'redflag',
+    location: '6.77772272, 992999992',
+    status: '',
+    images: ['image1', 'image2'],
+    videos: ['video1', 'video2'],
+    comment: 'A politician bribed me to keep quiet about his alleged embezzlement of three million dollars meant for building schools.',
+
+  },
+  {
+    id: 2,
+    createdOn: 'December 21, 2010',
+    createdBy: 'Ire Olaiya',
+    email: 'irefujel@gmail.com',
+    type: 'redflag',
+    location: '6.77772272,992999992',
+    status: '',
+    images: ['image1', 'image2'],
+    videos: ['video1', 'video2'],
+    comment: 'The money awarded for road construction was nowhere to be found.',
+  },
+];
+
+
+export default redflag;
