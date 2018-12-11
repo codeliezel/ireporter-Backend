71d2d275b65af7cda33af8faf6db158b99929d36
funmi5 <35759534+funmi5@users.noreply.github.com>
2018-12-11 13:02:58 +0100

    chore(updateUI): update all UI files
    - the blue link at the footer of the index page was changed so as to make it more visible
    
    - the signIn page form was reduced in size so as to make it responsive and better looking
    
    - the details needed on the signUp page was reduced so as to make 
       user sign up quick and easy, also the forms were reduced in size
    
    - the forms on the red-flag/intervention pages were reduced in size
      and less colours were used so as to make it better looking and less dramatic.
    
    - the userProfile page was re-designed and made use of tables
        for a better format instead of div boxes, which also includes the userRed-flag and userIntervention pages
    
    - the admin signIn page was also redesigned by reducing the size of the
       form to make it better looking
    
    - the admin page was also redesigned, by using tables for a better output format
    
    - generally, some elements were added to the css files so as to make the whole
      application responsive
    
    - the whole pages were remodelled to fit perfectly into the screen size, in order to
       eliminate the 1/2 inch space present before
    
    - external fonts were also made use of
    
    [Finishes #162460864]

--- UI/assets/js/navigation.js
+++ UI/assets/js/navigation.js
@@ -1,8 +1,18 @@
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function Password() {
+  const x = document.getElementById("pwd");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
